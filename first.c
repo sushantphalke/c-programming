7a7c6f8ab24e737439aeeb37fbf21e8155d0702c
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2021-12-30 10:37:22 +0530

    first.c added with good vibes to reherse c

---
+++ first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int a, b;
+  printf("enter number a: ");
+  scanf("%d", &a);
+  printf("enter number b: ");
+  scanf("%d", &b);
+  printf("the sum of a and b is :%d ", a + b);
+  return 0;
+}
