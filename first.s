9530a82b5cb46120307bc0fa964e79b0322907e3
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2021-12-30 12:38:33 +0530

    preprocessed outputs

---
+++ first.s
@@ -0,0 +1,54 @@
+	.file	"first.c"
+	.text
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+LC0:
+	.ascii "enter number a: \0"
+LC1:
+	.ascii "%d\0"
+LC2:
+	.ascii "enter number b: \0"
+LC3:
+	.ascii "the sum of a and b is :%d \0"
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB13:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	call	___main
+	movl	$LC0, (%esp)
+	call	_printf
+	leal	28(%esp), %eax
+	movl	%eax, 4(%esp)
+	movl	$LC1, (%esp)
+	call	_scanf
+	movl	$LC2, (%esp)
+	call	_printf
+	leal	24(%esp), %eax
+	movl	%eax, 4(%esp)
+	movl	$LC1, (%esp)
+	call	_scanf
+	movl	28(%esp), %edx
+	movl	24(%esp), %eax
+	addl	%edx, %eax
+	movl	%eax, 4(%esp)
+	movl	$LC3, (%esp)
+	call	_printf
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE13:
+	.ident	"GCC: (i686-posix-dwarf-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	_printf;	.scl	2;	.type	32;	.endef
+	.def	_scanf;	.scl	2;	.type	32;	.endef
