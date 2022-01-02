68c0999e40407c9b2a8b82f3ef41f9e34a2ecc8a
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-02 15:38:43 +0530

    comments and type formats

--- constantInC.c
+++ constantInC.c
@@ -4,5 +4,8 @@ const long s = 33;
 const long p = 34;
 
 int main() {
-  printf("sum of sp : %li", s + p);
+  printf("sum of sp : %li\n", s + p);
+  printf("my back slash \\n");
+  printf("this is tab \t horizonatal\n");
+  printf("heard a sound then programme finished\a");
 }
