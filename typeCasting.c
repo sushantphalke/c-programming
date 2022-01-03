054b068ff77928e87f44b2d96d27a0d8365997a4
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-03 08:35:31 +0530

    type casting

--- typeCasting.c
+++ typeCasting.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 int main() {
+  int a =33;
+  int b=34;
+  float c=(float)b/a;
+  printf("the value of operation is :%f\n",c);
+  return 0;
 }
+// type casting is simply conversion of one data type to another data type by assigning perticular data type before using it
