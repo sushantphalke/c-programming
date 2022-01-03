2d83e6607471f18dc5d5850ff61fba671aaf528f
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-03 09:04:25 +0530

    functions in c revision

--- functions.c
+++ functions.c
@@ -3,9 +3,13 @@
 float multiplication(float a, float b) {
   return a * b;
 }
+void alert() {
+  printf("alert function reached without arguments without return function");
+}
 int main() {
   float m = multiplication(3, 4);
   printf("%f\n", m);
-  printf("%f",multiplication(33,34));
+  printf("%f\n", multiplication(33, 34));
+  alert();
   return 0;
 }
