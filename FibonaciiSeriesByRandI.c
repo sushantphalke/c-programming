6f69d2867c97882664c4844950c61b0f9de7b095
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-04 14:35:42 +0530

    fibonacci number at position by recursive and iterative  method

--- FibonaciiSeriesByRandI.c
+++ FibonaciiSeriesByRandI.c
@@ -10,11 +10,21 @@ int fibonacci_recursive(int number){
   }
 
 }
+int fibonacci_iterative(int number){
+  int a=0;
+  int b=1;
+  for(int i=1;i<number;i++){
+    b=a+b;
+    a=b-a ;//numbers swaped
+  }
+  return  a;
+}
 
 int main(){
   int number;
   printf("Enter the number to get fibonacci series :");
   scanf("%d",&number);
-  printf("the value of fibonacci number at position no %d using interative approch is %d \n",number,fibonacci_recursive(number));
+  printf("the value of fibonacci number at position no %d using recursive approch is %d \n",number,fibonacci_recursive(number));
+  printf("the value of fibonacci number at position no %d using iterative approch is %d \n",number,fibonacci_iterative(number));
    return 0 ;
 }
