a24a91048cab11bf0bc4b68b2db5312d882bc2cc
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-04 15:32:01 +0530

    right staircase IMP  star patterns

--- starPatterns.c
+++ starPatterns.c
@@ -11,6 +11,20 @@ int main() {
     printf("\n");
   }
 
+  //******** IMP  ********
+  printf("right staircase\n");
+  int stairs = 8;
+  for (int i = 0; i <= stairs; i++) {
+    for (int j = 0; j <= stairs; j++) {
+      if (j >= stairs - i) {
+        printf("*");
+      } else {
+        printf(".");
+      }
+    }
+    printf("\n");
+  }
+
   printf("right hanger\n");
   for (int i = 0; i <= 8; i++) {
     for (int j = 0; j <= 8; j++) {
@@ -20,5 +34,6 @@ int main() {
     }
     printf("\n");
   }
+
   return 0;
 }
